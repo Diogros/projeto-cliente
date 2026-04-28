2ed9dd7df71dc835932538c25b8571843b7911c9
Diogros <henriquediogo730@gmail.com>
2026-04-28 15:40:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,35 +2,81 @@ const API = "http://localhost:3000";
 
 class SistemaClinica {
     constructor() {
+        document.getElementById('form-login').addEventListener('submit', (e) => this.autenticar(e));
+        
         this.iniciarEventos();
-        this.atualizarTabela();
     }
 
-    iniciarEventos() {
-        const form = document.getElementById('form-paciente');
-        const linkDash = document.getElementById('link-dashboard');
-        const linkCad = document.getElementById('link-cadastro');
+    async autenticar(evento) {
+        evento.preventDefault();
+        
+        const login = document.getElementById('login-usuario').value;
+        const senha = document.getElementById('senha-usuario').value;
+        const msgErro = document.getElementById('mensagem-erro-login');
 
-        if (form) {
-            form.addEventListener('submit', (e) => this.salvarPaciente(e));
-        }
-
-        if (linkDash) {
-            linkDash.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.navegar('tela-dashboard', 'link-dashboard');
-                this.atualizarTabela(); // Atualiza ao voltar para a lista
+        try {
+            const res = await fetch(`${API}/login`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ login, senha })
             });
-        }
 
-        if (linkCad) {
-            linkCad.addEventListener('click', (e) => {
-                e.preventDefault();
-                this.limparFormulario();
-                this.navegar('tela-cadastro', 'link-cadastro');
-            });
+            const dados = await res.json();
+
+            if (res.ok) {
+                document.getElementById('tela-login').style.display = 'none';
+                document.getElementById('sistema-principal').style.display = 'block';
+                this.atualizarTabela();
+            } else {
+                msgErro.innerText = dados.erro;
+                msgErro.style.display = 'block';
+            }
+        } catch (erro) {
+            console.error("Erro ao conectar com a API:", erro);
+            msgErro.innerText = "Servidor offline!";
+            msgErro.style.display = 'block';
         }
     }
+
+     iniciarEventos() {
+    const form = document.getElementById('form-paciente');
+    const linkDash = document.getElementById('link-dashboard');
+    const linkCad = document.getElementById('link-cadastro');
+    const inputCPF = document.getElementById('cpf');
+
+    if (form) {
+        form.addEventListener('submit', (e) => this.salvarPaciente(e));
+    }
+
+    if (linkDash) {
+        linkDash.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.navegar('tela-dashboard', 'link-dashboard');
+            this.atualizarTabela(); 
+        });
+    }
+
+    if (linkCad) {
+        linkCad.addEventListener('click', (e) => {
+            e.preventDefault();
+            this.limparFormulario();
+            this.navegar('tela-cadastro', 'link-cadastro');
+        });
+    }
+
+    if (inputCPF) {
+        inputCPF.addEventListener('input', (e) => {
+            document.getElementById('erro-cpf').style.display = 'none';
+            let v = e.target.value.replace(/\D/g, ""); 
+            if (v.length > 11) v = v.substring(0, 11);
+            v = v.replace(/(\d{3})(\d)/, "$1.$2");
+            v = v.replace(/(\d{3})(\d)/, "$1.$2");
+            v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+            
+            e.target.value = v;
+        });
+    }
+}
 
     navegar(idTela, idLink) {
         document.querySelectorAll('.tela').forEach(t => t.classList.remove('ativa'));
@@ -46,6 +92,15 @@ class SistemaClinica {
     async salvarPaciente(evento) {
         evento.preventDefault();
 
+        const cpfValor = document.getElementById('cpf').value;
+    const erroCPF = document.getElementById('erro-cpf');
+    if (!this.validarCPF(cpfValor)) {
+        erroCPF.style.display = 'block';
+        document.getElementById('cpf').focus();
+        return;
+    } else {
+        erroCPF.style.display = 'none';
+    }
         const idInput = document.getElementById('paciente-id').value;
         const dados = {
             nome: document.getElementById('nome').value,
@@ -84,9 +139,9 @@ class SistemaClinica {
         }
     }
 
-    async atualizarTabela() {
+    async atualizarTabela(ordenacao = 'nome') {
         try {
-            const res = await fetch(`${API}/cliente`);
+            const res = await fetch(`${API}/cliente?ordem=${ordenacao}`);
             const pacientes = await res.json();
 
             const tbody = document.querySelector('#tabela-pacientes tbody');
@@ -94,19 +149,25 @@ class SistemaClinica {
 
             tbody.innerHTML = '';
 
-            pacientes.forEach(p => {
-                const tr = document.createElement('tr');
-                tr.innerHTML = `
-                    <td><strong>${p.nome}</strong></td>
-                    <td>${p.cpf}</td>
-                    <td>${p.telefone}</td>
-                    <td>
-                        <button class="btn-acao btn-editar" onclick="instanciaApp.prepararEdicao(${p.id_clientes})">Editar</button>
-                        <button class="btn-acao btn-excluir" onclick="instanciaApp.excluirPaciente(${p.id_clientes})">Excluir</button>
-                    </td>
-                `;
-                tbody.appendChild(tr);
-            });
+pacientes.forEach(p => {
+    let cpfFormatado = p.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    let telFormatado = p.telefone;
+    if (telFormatado.length === 11) {
+        telFormatado = telFormatado.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    }
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+        <td><strong>${p.nome}</strong></td>
+        <td>${cpfFormatado}</td>
+        <td>${telFormatado}</td>
+        <td>
+            <button class="btn-acao btn-editar" onclick="instanciaApp.prepararEdicao(${p.id_clientes})">Editar</button>
+            <button class="btn-acao btn-excluir" onclick="instanciaApp.excluirPaciente(${p.id_clientes})">Excluir</button>
+        </td>
+    `;
+    tbody.appendChild(tr);
+});
 
             const mensagemVazia = document.getElementById('mensagem-vazia');
             const tabela = document.getElementById('tabela-pacientes');
@@ -169,6 +230,25 @@ class SistemaClinica {
         document.getElementById('paciente-id').value = "";
         document.getElementById('titulo-form').innerText = "Novo Paciente";
     }
+validarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, ''); 
+
+    if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
+
+    let soma = 0, resto;
+    for (let i = 1; i <= 9; i++) soma += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    resto = (soma * 10) % 11;
+    if ((resto === 10) || (resto === 11)) resto = 0;
+    if (resto !== parseInt(cpf.substring(9, 10))) return false;
+
+    soma = 0;
+    for (let i = 1; i <= 10; i++) soma += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    resto = (soma * 10) % 11;
+    if ((resto === 10) || (resto === 11)) resto = 0;
+    if (resto !== parseInt(cpf.substring(10, 11))) return false;
+
+    return true;
+}
 }
 
 const instanciaApp = new SistemaClinica();
