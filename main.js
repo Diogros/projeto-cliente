54253c13d1b5b288b56ee13a01d907e21de65834
Diogros <henriquediogo730@gmail.com>
2026-04-28 15:42:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
+        icon: path.join(__currentdir, 'icone.ico'),
         webPreferences: {
-            nodeIntegration: true
+            nodeIntegration: true,
+            contextIsolation: false
         }
     });
     win.loadFile('index.html');
+
 }
 
 app.whenReady().then(createWindow);
-
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
